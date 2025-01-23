cf065215ef0a22c8b602d30548bc5f70534d9256
Hayden-git <hayden.garry99@gmail.com>
2025-01-23 18:37:23 -0500

    implement first chart
    - create and implement Doughnut Chart component from Chart.js

--- banking_app/components/ui/TotalBalanceBox.tsx
+++ banking_app/components/ui/TotalBalanceBox.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import AnimatedCounter from "./AnimatedCounter";
+import DoughnutChart from "./DoughnutChart";
 
 // import { formatAmount } from "@/lib/utils";
 
-const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
+const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotalBalanceBoxProps) => {
 	return (
 		<section className="total-balance">
-			<div className="total-balance-chart">{/* Doughnut Chart */}</div>
+			<div className="total-balance-chart">
+				<DoughnutChart accounts={accounts} />
+			</div>
 
 			<div className="flex flex-col gap-6">
 				<h2 className="header-2">{totalBanks} Bank Accounts</h2>
@@ -14,11 +17,11 @@ const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: Tot
 				<div className="flex flex-col gap-2">
 					<p className="total-balance-label">Total Current Balance</p>
 
-					<p className="total-balance-amount flex-center gap-2">
+					<div className="total-balance-amount flex-center gap-2">
 						<AnimatedCounter amount={totalCurrentBalance} />
 
 						{/* {formatAmount(totalCurrentBalance)} */}
-					</p>
+					</div>
 				</div>
 			</div>
 		</section>
