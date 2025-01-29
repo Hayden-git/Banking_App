b33d100dfb8f85f9b5240d4c6e6cd30cf4a9e9a3
Hayden-git <hayden.garry99@gmail.com>
2025-01-29 17:37:28 -0500

    Fix styling typo
    - Fix styling typo on BankCard.tsx component

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -18,10 +18,10 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
 					</div>
 
 					<article className="flex flex-col gap-2">
-						<div className="flex justift-between">
+						<div className="flex justify-between">
 							<h1 className="text-12 font-semibold text-white">{userName}</h1>
 
-							<h2 className="text-12 font-semibold text-white pl-1">●● / ●●</h2>
+							<h2 className="text-12 font-semibold text-white">●● / ●●</h2>
 						</div>
 
 						<p className="text-14 font-semibold tracking-[1.1px] text-white">
