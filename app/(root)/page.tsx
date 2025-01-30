bdf9e71d8ca7eb7121c027c7867c3bf4bda6e137
Hayden-git <hayden.garry99@gmail.com>
2025-01-30 16:58:39 -0500

    Access user data when logged in
    - dynamically show logged in user's data on main site after auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import RightSidebar from "@/components/ui/RightSidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = () => {
-	const loggedIn = { firstName: "Hayden", lastName: "Garry", email: "asdf@gmail.com" };
+const Home = async () => {
+	const loggedIn = await getLoggedInUser();
 
 	return (
 		<section className="home">
@@ -14,7 +15,7 @@ const Home = () => {
 					<HeaderBox
 						type="greeting"
 						title="Welcome"
-						user={loggedIn?.firstName || "Guest"}
+						user={loggedIn?.name || "Guest"}
 						subtext="Access and manage your financials in one place"
 					/>
 
