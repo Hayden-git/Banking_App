52f8184264bb3cfa047f483be86d35f42d3de94d
Hayden-git <hayden.garry99@gmail.com>
2025-02-01 20:43:49 -0500

    Implement auth, appwrite, and sentry
    implement auth, appwrite, and sentry

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,38 +1,23 @@
+import Image from "next/image";
+
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return <main>{children}</main>;
+	return (
+		<main className="flex min-h-screen w-full justify-between font-inter">
+			{children}
+			<div className="auth-asset">
+				<div>
+					<Image
+						src="/icons/auth-image.svg"
+						alt="Auth image"
+						width={500}
+						height={500}
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
-
-// import type { Metadata } from "next";
-// import localFont from "next/font/local";
-// import { Inter, IBM_Plex_Serif } from "next/font/google";
-// import "./globals.css";
-
-// const geistSans = localFont({
-// 	src: "./fonts/GeistVF.woff",
-// 	variable: "--font-geist-sans",
-// 	weight: "100 900",
-// });
-// const geistMono = localFont({
-// 	src: "./fonts/GeistMonoVF.woff",
-// 	variable: "--font-geist-mono",
-// 	weight: "100 900",
-// });
-
-// const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-// const ibmPlexSerif = IBM_Plex_Serif({
-// 	subsets: ["latin"],
-// 	weight: ["400"],
-// 	variable: "--font-ibm-plex-serif",
-// });
-
-// export const metadata: Metadata = {
-// 	title: "Brink",
-// 	description: "Brink is a modern banking platform.",
-// 	icons: {
-// 		icon: "/icons/logo.svg",
-// 	},
-// };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
+// import "./darkstyles.css";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
