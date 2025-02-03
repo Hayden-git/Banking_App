e770353b45e329c0db8561ca019ae6b90346f04a
Hayden-git <hayden.garry99@gmail.com>
2025-02-03 00:19:49 -0500

    Move components out of ui folder
    - Move components out of ui folder

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState, useEffect } from "react";
 
-import { Button } from "./button";
+import { Button } from "./ui/button";
 
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from "react-plaid-link";
 
